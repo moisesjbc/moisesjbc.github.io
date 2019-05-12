0ceff68c2e89ca3ff46566fd4038a9d11c807932
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-05-12 19:20:39 +0100

    Added empty components for Home and Games

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GamesComponent } from './games/games.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'games', component: GamesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

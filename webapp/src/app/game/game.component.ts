1dce76c86750e506ed4e9bfc0364e607491b3e8f
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-05-15 19:34:43 +0100

    Added page about game Administrathor

--- webapp/src/app/game/game.component.ts
+++ webapp/src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -10,8 +11,11 @@ export class GameComponent implements OnInit {
 
     gameData : any;
 
-    constructor(private http: HttpClient) {
-        this.http.get('./assets/db/games/elf-hunting/info.json').subscribe(gameData => {
+    constructor(private http: HttpClient, private route: ActivatedRoute) {
+        const id = this.route.snapshot.paramMap.get('id');
+        console.log('id');
+        console.log(id);
+        this.http.get('./assets/db/games/' + id + '/info.json').subscribe(gameData => {
             this.gameData = gameData;
             console.log('gameData');
             console.log(JSON.stringify(gameData));

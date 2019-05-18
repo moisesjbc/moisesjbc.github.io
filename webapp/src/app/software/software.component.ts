729d4bb3d334e99500831d5b78fbb6bbcb260db3
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-05-18 13:03:12 +0100

    Added software summaries to /software

--- webapp/src/app/software/software.component.ts
+++ webapp/src/app/software/software.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-software',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./software.component.css']
 })
 export class SoftwareComponent implements OnInit {
+  private softwareIds: string[] = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('./assets/db/software/index.json').subscribe(softwareData => {
+      console.log('softwareData');
+      console.log(softwareData);
+      this.softwareIds = softwareData['software'];
+    });
   }
 
 }

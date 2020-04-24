9ae68a525883f415d694cd68f0826ad883268298
Moisés J <moisesjbc@gmail.com>
2020-04-24 18:28:56 +0100

    Added public data + home page info

--- webapp/src/sections/home/Home.tsx
+++ webapp/src/sections/home/Home.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import data from '../../db/home/home.json';
+import './Home.css';
 
 function Home() {
-    return (<p>Home</p>);
+    return (
+        <>
+            <ReactMarkdown source={data['about_me']}/>
+            <ul id="news">
+            {data['news'].map((siteNew, index) => (
+                <li>
+                    <strong>{siteNew['date']}: </strong>
+                    <ReactMarkdown source={siteNew['body']}/>
+                </li>
+            ))}
+            </ul>
+        </>
+    );
 }
 
 export default Home;

1e1323c93b646c11c35cd7fcdb1d0aacc78375c3
Moisés J <moisesjbc@gmail.com>
2020-05-09 13:32:13 +0100

    Added 'starred' projects + removed book / short-story division in /literature

--- webapp/src/components/projects.list/ProjectsList.tsx
+++ webapp/src/components/projects.list/ProjectsList.tsx
@@ -5,23 +5,50 @@ import ContentHeader from '../../components/content.header/ContentHeader';
 
 function ProjectsList(props: any) {
     let {projectsType, projectIds, headersLevel = 2} = props;
-    let [projectsData, setProjectsData] = useState<Array<any>>([]);
+    let [projectsData, setProjectsData] = useState<any>({
+        starred: [],
+        others: []
+    });
     let [tagIds, setTagsIds] = useState<Array<string>>([]);
     let [selectedTagIds, setSelectedTagIds] = useState<Array<string>>([]);
 
     useEffect(() => {
-        let data: Array<any> = [];
+        let data: any = {
+            starred: [],
+            others: []
+        };
         projectIds.forEach((projectId: string) => {
-            data.push({
+            const projectData = {
                 ...require(`../../db/${projectsType}/${projectId}`),
                 id: projectId
-            });
+            };
+
+            if (projectData.starred) {
+                data.starred.push(projectData);
+            } else {
+                data.others.push(projectData);
+            }
         });
         setProjectsData(data);
+
+        console.log('data', data);
 
         let tags = require(`../../db/${projectsType}/tags.json`)['tags'];
         setTagsIds(Object.keys(tags));
     }, [projectsType, projectIds]);
+
+    const displayProjects = (title: string, projects: Array<any>) => (
+        <>
+            <h2>{ title }</h2>
+            <div className="row">
+                {projects
+                    .filter((projectData) => !selectedTagIds.length || selectedTagIds.every(selectedTagId => projectData.tags.includes(selectedTagId)))
+                    .map((projectData, index) => (
+                        <ProjectSummary key={index} projectSubDir={projectsType} projectData={projectData} headerLevel={headersLevel} />
+                    ))}
+            </div>
+        </>
+    )
 
     return (
         <>
@@ -30,13 +57,8 @@ function ProjectsList(props: any) {
                 <p>Filtrar por etiquetas</p>
                 <TagsLink projectsType={projectsType} tagIds={tagIds} selectedTagIds={selectedTagIds} setSelectedTagIds={setSelectedTagIds} />
             </div>
-            <div className="row">
-                {projectsData
-                    .filter((projectData) => !selectedTagIds.length || selectedTagIds.every(selectedTagId => projectData.tags.includes(selectedTagId)))
-                    .map((projectData, index) => (
-                        <ProjectSummary key={index} projectSubDir={projectsType} projectData={projectData} headerLevel={headersLevel} />
-                    ))}
-            </div>
+            { displayProjects('Descatados', projectsData.starred) }
+            { displayProjects('Otros', projectsData.others) }
         </>
     );
 }
--- webapp/src/sections/literature/Literature.tsx
+++ webapp/src/sections/literature/Literature.tsx
@@ -5,16 +5,7 @@ import index from '../../db/literature/index.json';
 function Literature() {
     return (
         <>
-            <h2>Libros</h2>
-            <ProjectsList projectsType="literature" projectIds={index['books']} headersLevel={3} />
-
-            <h2>Relatos cortos</h2>
-            {index['short-stories'].map((periodShortStories: any, index: number) => (
-                <section key={`''+${index}`}>
-                    <h3>{ periodShortStories.year }</h3>
-                    <ProjectsList projectsType="literature" projectIds={periodShortStories['short-stories']} headersLevel={4} />
-                </section>
-            ))}
+            <ProjectsList projectsType="literature" projectIds={index['literature']} />
         </>
     );
 }

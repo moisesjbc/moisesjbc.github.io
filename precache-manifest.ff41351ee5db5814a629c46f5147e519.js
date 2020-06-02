3d2d926fec877a081d4fd316cdedd19062da11c2
Moisés J <moisesjbc@gmail.com>
2020-06-02 18:28:05 +0100

    Added missing files to build

---
+++ precache-manifest.ff41351ee5db5814a629c46f5147e519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eea2ba9282f14061b4eb85fc0582467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d439e71c418b016b44a",
+    "url": "/static/css/main.0ebdb060.chunk.css"
+  },
+  {
+    "revision": "294ac81a2a3401b77bb6",
+    "url": "/static/js/2.c7242188.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.c7242188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d439e71c418b016b44a",
+    "url": "/static/js/main.afa8a843.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

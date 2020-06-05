eff49ef8859d0268976aaf448870ac9af581da53
Moisés J <moisesjbc@gmail.com>
2020-06-05 17:07:42 +0100

    Updated build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff41351ee5db5814a629c46f5147e519.js"
+  "/precache-manifest.a6746be3db62c8cdb993e9cbc99b15d3.js"
 );
 
 self.addEventListener('message', (event) => {

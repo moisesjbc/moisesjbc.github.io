73c9821e892b23473527d8b3b75f265d1e35e587
Moisés J <moisesjbc@gmail.com>
2020-10-04 18:01:11 +0100

    Added metadata for UFO taxi!

---
+++ precache-manifest.b72767f8b519bf5ea850deb390f08027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b93552a84c356a8e7e51344a49fcca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162835a2aa9f3e255feb",
+    "url": "/static/css/main.77340125.chunk.css"
+  },
+  {
+    "revision": "14a0d7dd623c5f8f46f5",
+    "url": "/static/js/2.d8343ebc.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.d8343ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162835a2aa9f3e255feb",
+    "url": "/static/js/main.b6342697.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

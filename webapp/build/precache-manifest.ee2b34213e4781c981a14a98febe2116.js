001d237e9c2545e7e675b116baa8b471242ca3b6
Moisés J <moisesjbc@gmail.com>
2020-10-11 13:36:20 +0100

    Updated build

---
+++ webapp/build/precache-manifest.ee2b34213e4781c981a14a98febe2116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fc005ee608b0b9f352575d1dd0594d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb75f724bc7c8aef8a67",
+    "url": "/static/css/main.5ad561cf.chunk.css"
+  },
+  {
+    "revision": "0459e7e108da875a8374",
+    "url": "/static/js/2.58aea7b3.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.58aea7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb75f724bc7c8aef8a67",
+    "url": "/static/js/main.44c2fa19.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

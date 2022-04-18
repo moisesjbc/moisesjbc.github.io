3e2a8ef08bc0625d1f2accdf3ef994f48c7bab12
Moisés J <moisesjbc@gmail.com>
2022-04-18 20:31:26 +0100

    DIST - Añadido "Hold it!"

---
+++ webapp/build/precache-manifest.114dc739ea792823ff69a03b0064e47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52ebf9afeeb9ad46125882d5d37666f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a3b6edefe736f06a59",
+    "url": "/static/css/main.f653020e.chunk.css"
+  },
+  {
+    "revision": "a1b750e53cfe71072806",
+    "url": "/static/js/2.7189f043.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.7189f043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a3b6edefe736f06a59",
+    "url": "/static/js/main.b48660f7.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

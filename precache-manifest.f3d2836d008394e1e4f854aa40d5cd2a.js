d2f5a456e78c223de767a819e6f92614bb4d7fa0
Moisés J <moisesjbc@gmail.com>
2023-02-06 23:48:32 +0000

    DIST - Añade v0.4 de "Contando hasta 100"

---
+++ precache-manifest.f3d2836d008394e1e4f854aa40d5cd2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8a2e4b177229e9d5d0c980e6baae05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d80275e62a87ef5238",
+    "url": "/static/css/main.e5a263bf.chunk.css"
+  },
+  {
+    "revision": "9e5de885f8fc6d058107",
+    "url": "/static/js/2.a0afa7d5.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.a0afa7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d80275e62a87ef5238",
+    "url": "/static/js/main.02f4b49e.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

8ee78f07ed9ae6976f9a788c0057f588908cce3d
Moisés J <moisesjbc@gmail.com>
2023-06-18 16:28:52 +0100

    DIST - Añade juego "Zero gravity deivery"

---
+++ precache-manifest.6d9aadabdb9fc10002ae9fd1828e46e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b770fc1151c4c33d8d8c63dff78d54b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30657267b1770234c296",
+    "url": "/static/css/main.39fd079b.chunk.css"
+  },
+  {
+    "revision": "bda2004fbe06fa5f7b82",
+    "url": "/static/js/2.0a2a501a.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.0a2a501a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30657267b1770234c296",
+    "url": "/static/js/main.ac41d538.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

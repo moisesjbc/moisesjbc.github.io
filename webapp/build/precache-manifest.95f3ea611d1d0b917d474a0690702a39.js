dc36a31221a60ceb486537b0e1c790669d4232e7
Moisés J <moisesjbc@gmail.com>
2023-10-17 21:03:42 +0100

    DIST - Añade videojuego "¡A la cama!" (v0.2)

---
+++ webapp/build/precache-manifest.95f3ea611d1d0b917d474a0690702a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffd64517eab88fe35447634a619306a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c265b34e58d6af2722",
+    "url": "/static/css/main.5a01bec9.chunk.css"
+  },
+  {
+    "revision": "15f7649acfac3c3caa02",
+    "url": "/static/js/2.cb3e7347.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.cb3e7347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c265b34e58d6af2722",
+    "url": "/static/js/main.74add758.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);

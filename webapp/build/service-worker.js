65ead5b7c7b24300cd466435f7d7449128658270
Moisés J <moisesjbc@gmail.com>
2024-10-12 13:47:04 +0100

    Añade juego "Tiny revolution" (DIST)

--- webapp/build/service-worker.js
+++ webapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6619f1a7be70d6792e182307f04043b6.js"
+  "/precache-manifest.2860005d9d80e8c436d5ecf8a60a783c.js"
 );
 
 self.addEventListener('message', (event) => {

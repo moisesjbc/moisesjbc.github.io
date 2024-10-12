2f559eaa86cab8b1088adb5696626355f013cc07
Moisés J <moisesjbc@gmail.com>
2024-10-12 13:47:04 +0100

    Añade juego "Tiny revolution" (DIST)

---
+++ precache-manifest.2860005d9d80e8c436d5ecf8a60a783c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca160816221526351837b54a2bddfd87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "846b296736f1f7c76129",
+    "url": "/static/css/main.5a01bec9.chunk.css"
+  },
+  {
+    "revision": "440cba9ea52ee9feae55",
+    "url": "/static/js/2.2dbf9193.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.2dbf9193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846b296736f1f7c76129",
+    "url": "/static/js/main.f401e560.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);
